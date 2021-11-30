f4d8066ae882b2f1aaedf65083d0bdded3e51d1d
Abdykapar Kamilov <abdykaparkamilov@mail.ru>
2021-11-30 13:43:13 +0600

    refactor category section

--- components/index/CategorySection.vue
+++ components/index/CategorySection.vue
@@ -1,67 +1,156 @@
 <template>
 	<section class="category section">
 		<div class="container">
-			<h2 class="category__main__title">Browse programs by category</h2>
-			
-			<button class="primary-btn blue category__all-btn">All Categories</button>
+			<h2 class="category__title">Browse programs by category</h2>
+			<div class="category__body">
+				<div v-for="i in categories" :key="i.icon" class="category__item">
+					<i class="category__icon" :class="i.icon"></i> <span>{{i.name}}</span>
+				</div>
+			</div>
+			<div class="category__space"></div>
+			<h2 class="category__title">Browse programs by level</h2>
+			<div class="category__body">
+				<div v-for="i in programs" :key="i" class="category__item">
+					<span class="category__btn">
+						<i class="ti-plus"></i>
+					</span>
+					<span>{{i}}</span>
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script>
 export default {
-	name: 'CategorySection'
+	name: 'CategorySection',
+	data () {
+		return {
+			categories: [
+				{
+					name: 'Art & Design',
+					icon: 'ti-palette'
+				},
+				{
+					name: 'Business',
+					icon: 'ti-briefcase'
+				},
+				{
+					name: 'Computer Science',
+					icon: 'ti-desktop'
+				},
+				{
+					name: 'Engineering',
+					icon: 'ti-settings'
+				},
+				{
+					name: 'General Studies',
+					icon: 'ti-book'
+				},
+				{
+					name: 'Medicine & Health',
+					icon: 'ti-heart-broken'
+				},
+				{
+					name: 'Humanities',
+					icon: 'ti-world'
+				},
+				{
+					name: 'Languages',
+					icon: 'ti-comments-smiley'
+				},
+				{
+					name: 'Law',
+					icon: 'ti-hummer'
+				},
+				{
+					name: 'Media',
+					icon: 'ti-blackboard'
+				},
+				{
+					name: 'Science',
+					icon: 'ti-paint-bucket'
+				},
+				{
+					name: 'Teaching',
+					icon: 'ti-notepad'
+				},
+				{
+					name: 'Tourism & Hospitality',
+					icon: 'ti-direction-alt'
+				},
+				{
+					name: 'Transport & Logistics',
+					icon: 'ti-truck'
+				},
+				{
+					name: 'Online programs',
+					icon: 'ti-rss-alt'
+				},
+			],
+			programs: [
+				'Bachelor’s degree',
+				'Master’s degree',
+				'Doctorate PHP',
+				'MBA',
+				'Graduate diploma',
+				'Diploma/ certificate',
+				'Summer Short course',
+				'Foundation program',
+				'Associate‘s degree'
+			]
+		}
+	}
 }
 </script>
 
 <style lang="scss">
     .category {
-		margin: 140px 0 125px;
-		&__content {
-			display: grid;
-			grid-template-columns: repeat(5, 1fr);
-			grid-template-rows: 1fr 1fr 1fr 1fr;
-			column-gap: 20px;
-			row-gap: 14px;
-		}
+		margin: 75px 0;
 		&__title {
 			z-index: 1;
-			font-size: 48px;
-			line-height: 56px;
+			font-weight: 500;
+			font-size: 24px;
+			line-height: 28px;
 			color: #00409F;
-			margin-left: 9px;
+		}
+		&__body {
+			display: grid;
+			grid-template-columns: repeat(3, 1fr);
+			margin: 28px 0;
 		}
 		&__item {
-			position: relative;
+			min-width: 250px;
 			display: flex;
-			justify-content: center;
 			align-items: center;
-			height: 338px;
-			background-repeat: no-repeat;
-			background-size: cover;
-			grid-column: span 3;
+			font-weight: 500;
+			font-size: 24px;
+			line-height: 28px;
+			color: #3F3F3F;
+			margin: 18px 0;
+		}
+		&__icon {
+			margin-right: 25px;
+			color: #00409F;
+			font-size: 33px;
+		}
+		&__btn {
+			width: 30px;
+			height: 30px;
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			background: #818181;
+			border-radius: 5px;
+			margin-right: 25px;
 
-			&.join-row {
-				grid-row: span 2;
-				grid-column: span 2;
-				height: 100%;
+			> i {
+				color: #ffffff;
+				font-size: 20px;
 			}
 		}
-		&__main {
-			&__title {
-				font-size: 36px;
-				line-height: 42px;
-				color: var(--main-primary);
-			}
-		}
-		&__all-btn {
-			margin-top: 33px;
-		}
-		&__backdrop {
-			position: absolute;
-			background: rgba(255, 255, 255, 0.52);
-			height: 100%;
-			width: 100%;
+		&__space {
+			height: 40px;
 		}
 	}
 </style>
